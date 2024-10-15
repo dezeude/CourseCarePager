d0b5d3328f070a1244ed39222c68ffceb11ab19d
dezeude <dezeude@unc.edu>
2024-10-15 11:39:57 -0400

    Think it works, haven't fully tested yet

--- panel.js
+++ panel.js
@@ -1,14 +1,20 @@
-chrome.devtools.network.onRequestFinished.addListener((request) => {
-  const div = document.createElement("div");
-  div.innerHTML = request.request.url;
-  div.style.background = "green";
-  document.body.appendChild(div);
-  console.log(request);
+let queue = [];
+let audio = new Audio("ping.mp3");
 
-  request.getContent((content) => {
-    console.log(content);
+chrome.devtools.network.onRequestFinished.addListener((request) => {
+  request.getContent(async (content) => {
+    let json = await JSON.parse(content);
+    let data = json[0];
+    console.log(data);
+    if (data.queue.length > queue.length) {
+      audio.play();
+      const div = document.createElement("div");
+      div.style.background = "green";
+      let q = data.queue;
+      let ticket = q.pop();
+      div.innerHTML = `${ticket.holder.firstName} has joined the queue!`;
+      document.body.appendChild(div);
+    }
+    queue = [...data.queue];
   });
 });
-// chrome.devtools.network.getHAR((harLog) => {
-//     console.log()
-// })
